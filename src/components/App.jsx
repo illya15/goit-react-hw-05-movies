9b9120a19810dec4ec3cc3299913f39960532ad0
illya15 <113913094+illya15@users.noreply.github.com>
2023-06-01 13:11:35 +0300

    hw5 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,20 @@
-
+import Home from "pages/Home";
+import { NavLink,Route,Routes } from "react-router-dom";
 // API key 9134aaf09b2c139a92831aec99413f4a
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/movies">Colection</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/movies" element={<div>Movies</div>}></Route>
+        <Route path="/movies/:movieId" element={<div>MoviesDetal</div>}></Route>
+      </Routes>
     </div>
   );
 };
+
